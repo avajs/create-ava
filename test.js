216c748e349000cdd245e2d53b4f5fcd04a28ea3
Sindre Sorhus <sindresorhus@gmail.com>
2015-09-22 19:07:05 +0700

    ES2015ify tests

--- test.js
+++ test.js
@@ -1,102 +1,100 @@
-'use strict';
-var path = require('path');
-var fs = require('fs');
-var tempWrite = require('temp-write');
-var dotProp = require('dot-prop');
-var test = require('ava');
-var fn = require('./');
-var originalArgv = process.argv.slice();
-var get = dotProp.get;
+import path from 'path';
+import fs from 'fs';
+import tempWrite from 'temp-write';
+import dotProp from 'dot-prop';
+import test from 'ava';
+import fn from './';
+
+const originalArgv = process.argv.slice();
+const get = dotProp.get;
 
 function run(pkg) {
-	var filepath = tempWrite.sync(JSON.stringify(pkg), 'package.json');
+	const filepath = tempWrite.sync(JSON.stringify(pkg), 'package.json');
 
 	return fn({
 		cwd: path.dirname(filepath),
 		skipInstall: true
-	}).then(function () {
-		return JSON.parse(fs.readFileSync(filepath, 'utf8'));
-	});
+	}).then(() => JSON.parse(fs.readFileSync(filepath, 'utf8')));
 }
 
-test('empty package.json', function (t) {
-	return run({}).then(function (pkg) {
+test('empty package.json', t => {
+	return run({}).then(pkg => {
 		t.is(get(pkg, 'scripts.test'), 'ava');
 	});
 });
 
-test('has scripts', function (t) {
+test('has scripts', t => {
 	return run({
 		scripts: {
 			start: ''
 		}
-	}).then(function (pkg) {
+	}).then(pkg => {
 		t.is(get(pkg, 'scripts.test'), 'ava');
 	});
 });
 
-test('has default test', function (t) {
+test('has default test', t => {
 	return run({
 		scripts: {
 			test: 'echo "Error: no test specified" && exit 1'
 		}
-	}).then(function (pkg) {
+	}).then(pkg => {
 		t.is(get(pkg, 'scripts.test'), 'ava');
 	});
 });
 
-test('has only AVA', function (t) {
+test('has only AVA', t => {
 	return run({
 		scripts: {
 			test: 'ava'
 		}
-	}).then(function (pkg) {
+	}).then(pkg => {
 		t.is(get(pkg, 'scripts.test'), 'ava');
 	});
 });
 
-test('has test', function (t) {
+test('has test', t => {
 	return run({
 		scripts: {
 			test: 'foo'
 		}
-	}).then(function (pkg) {
+	}).then(pkg => {
 		t.is(get(pkg, 'scripts.test'), 'foo && ava');
 	});
 });
 
-test('has cli args', function (t) {
+test('has cli args', t => {
 	process.argv = originalArgv.concat(['--init', '--foo']);
 
 	return run({
 		scripts: {
 			start: ''
 		}
-	}).then(function (pkg) {
+	}).then(pkg => {
 		process.argv = originalArgv;
 		t.is(get(pkg, 'scripts.test'), 'ava --foo');
 	});
 });
 
-test('has cli args and existing binary', function (t) {
+test('has cli args and existing binary', t => {
 	process.argv = originalArgv.concat(['--init', '--foo', '--bar']);
 
 	return run({
 		scripts: {
 			test: 'foo'
 		}
-	}).then(function (pkg) {
+	}).then(pkg => {
 		process.argv = originalArgv;
 		t.is(get(pkg, 'scripts.test'), 'foo && ava --foo --bar');
 	});
 });
 
-test('installs the AVA dependency', function (t) {
-	var filepath = tempWrite.sync(JSON.stringify({}), 'package.json');
+test('installs the AVA dependency', t => {
+	const filepath = tempWrite.sync(JSON.stringify({}), 'package.json');
 
 	return fn({
 		cwd: path.dirname(filepath)
-	}).then(function () {
+	}).then(() => {
 		t.ok(get(JSON.parse(fs.readFileSync(filepath, 'utf8')), 'devDependencies.ava'));
 	});
 });
